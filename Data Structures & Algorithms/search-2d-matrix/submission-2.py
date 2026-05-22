ea2421360b147e72e132a8f4c124345a86bc8af2
Priya Khradia <khradiapriya@gmail.com>
2026-05-22 15:33:26 +0530

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.py
@@ -0,0 +1,43 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # for i in range(len(matrix)):
+        #     low = 0
+        #     high =len(matrix[i])-1
+        #     while low<= high:
+        #         if target > matrix[i][high]:
+        #             break
+        #         elif target == matrix[i][high]:
+        #             return True
+        #         elif target == matrix[i][low]:
+        #             return True
+        #         else:
+        #             mid = (low+high)//2
+        #             if target == matrix[i][mid]:
+        #                 return True
+        #             elif target > matrix[i][mid]:
+        #                 low = mid+1
+        #             else:
+        #                 high = mid-1
+        # return False
+
+        for i in range(len(matrix)):
+            low = 0
+            high = len(matrix[i]) - 1
+            
+            # Fast skip: If the target can't possibly fit in this row, 
+            # don't even enter the while loop. Move to the next row.
+            if target < matrix[i][low] or target > matrix[i][high]:
+                continue
+                
+            # Standard, bulletproof binary search
+            while low <= high:
+                mid = (low + high) // 2
+                
+                if matrix[i][mid] == target:
+                    return True
+                elif matrix[i][mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+                    
+        return False
